fee741088d3e897ab35d738e130dae91ebaf3f46
Samit Pal <samit04@gmail.com>
2016-09-06 20:53:01 +0530

    Add scripts

---
+++ scripts/push_alert_config.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import os
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config_file', help='config file to push.')
+parser.add_argument('-s', '--consul_host_port', help='consul host address:port.', default='localhost:8500')
+args = parser.parse_args()
+
+CONSUL_BASE_URL = 'http://%s/v1/kv/' % args.consul_host_port
+CONSUL_BASE_KEY = 'monitoring/configs/alerts'
+
+def main():
+  base_file_name = os.path.basename(args.config_file)
+  print '=== About to push configs for %s service ===' % base_file_name
+  consul_key_path = CONSUL_BASE_URL+CONSUL_BASE_KEY+'/'+base_file_name
+  print '=== Pushing to consul key %s ===' % consul_key_path
+ 
+  data = None
+  with open(args.config_file) as f:
+    data = f.read()
+
+  url = consul_key_path
+  r = requests.put(url, data=data)
+  if r.status_code != 200:
+    print 'Return status is non 200.'  
+    return
+  print 'Done.'
+
+if __name__ == '__main__':
+ main()
